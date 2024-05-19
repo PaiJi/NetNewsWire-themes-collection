fcfe61ce6de6b7d7103bd705097bcbd608084c31
JiPai <github@jipai.moe>
2024-05-20 00:08:17 +0800

    fix: fix image url path

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -4,10 +4,9 @@ const GITHUB_REPO_STAR_BADGE = "https://img.shields.io/github/stars";
 export function getRepoStarBadge(repoUrl: string) {
   try {
     const spliteRepoUrl = repoUrl.replace("https://", "").split("/");
-    console.log(spliteRepoUrl);
-    return `${GITHUB_REPO_STAR_BADGE}/${
-      spliteRepoUrl[1]
-    }/${spliteRepoUrl[2].split("#")[0]}?style=flat&logo=github`;
+    return `${GITHUB_REPO_STAR_BADGE}/${spliteRepoUrl[1]}/${
+      spliteRepoUrl[2].split("#")[0]
+    }?style=flat&logo=github`;
   } catch (error) {
     return null;
   }
