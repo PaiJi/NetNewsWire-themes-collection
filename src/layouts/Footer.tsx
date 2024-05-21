7076e3e1638f270e71fb37616ee502bbb3d5f9c1
JiPai <github@jipai.moe>
2024-05-22 03:35:05 +0800

    feat: add RSS icon

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,12 +1,19 @@
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub, FaRss } from "react-icons/fa6";
 
 function Footer() {
   return (
     <footer className="mt-6 py-6 border-t flex justify-between">
       <span className="text-sm">NetNewsWire Themes collection</span>
-      <div>
+      <div className="flex items-center">
         <a
-          className="transition hover:text-link flex items-center text-sm"
+          className="transition hover:text-[#f26522] flex items-center text-sm"
+          href={`https://github.com/PaiJi/NetNewsWire-themes-collection/rss.xml`}
+        >
+          <FaRss className="inline-block mr-1" /> RSS
+        </a>
+
+        <a
+          className="ml-4 transition hover:text-link flex items-center text-sm"
           href="https://github.com/PaiJi/NetNewsWire-themes-collection"
         >
           <FaGithub className="inline-block mr-1" /> GitHub
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub, FaRss } from "react-icons/fa6";
 
 function Header() {
   return (
@@ -13,12 +13,19 @@ function Header() {
         themes collection
       </h1>
 
-      <div className="text-2xl">
+      <div className="text-2xl flex items-center">
         <a
           href="https://github.com/PaiJi/NetNewsWire-themes-collection"
           className="hover:text-link transition"
         >
           <FaGithub />
+        </a>
+
+        <a
+          className="ml-8 transition hover:text-[#f26522] flex items-center"
+          href={`https://github.com/PaiJi/NetNewsWire-themes-collection/rss.xml`}
+        >
+          <FaRss className="inline-block mr-1" />
         </a>
       </div>
     </header>
